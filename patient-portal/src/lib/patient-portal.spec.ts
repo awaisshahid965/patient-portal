b3c8529e2bc00d465dbaf547627366c06efc72a1
Awais Shahid <awaisshahid965@gmail.com>
2024-07-08 11:20:17 +0500

    feat(nx): Added Nx Cloud token to your nx.json
              To connect your workspace to Nx Cloud, push your repository
              to your git hosting provider and go to the following URL:
    
              https://cloud.nx.app/connect/o17sfcQxaG

---
+++ patient-portal/src/lib/patient-portal.spec.ts
@@ -0,0 +1,7 @@
+import { patientPortal } from './patient-portal';
+
+describe('patientPortal', () => {
+  it('should work', () => {
+    expect(patientPortal()).toEqual('patient-portal');
+  });
+});
