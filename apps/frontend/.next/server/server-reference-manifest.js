49ca477dc6d6bddd388ba5ead76d1564ced6576d
Awais Shahid <awaisshahid965@gmail.com>
2024-07-08 14:54:33 +0500

    feat: initialized monorepo nextjs structure

---
+++ apps/frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8YknnrFG4PigWZn9k4ewJ6uQ7Xvd0XNZ3gNr9wAXq8E=\"\n}"
---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import TestComponent from "@patient-portal/ui-components/test"
+
+export default function Home() {
+  return (
+    <div>
+      <p>App initialized.</p>
+      <TestComponent />
+    </div>
+  );
+}
